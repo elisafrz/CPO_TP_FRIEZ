bd2b1ca38bc00f8114e1a6a2a7516f33098976d5
Elisa Friez <elisa.friez@epfedu.fr>
2023-11-06 15:41:28 +0100

    La classe CelluleLumineuse a bien été ajoutée et elle est fonctionnelle. La classe GrilleDeCellules a bien été ajoutée et est fonctionnelle.

---
+++ LightsOff_FRIEZ_version_console/src/lightsoff_friez_version_console/GrilleDeCellules.java
@@ -0,0 +1,108 @@
+package lightsoff_friez_version_console;
+
+
+import java.util.Random;
+import lightsoff_friez_version_console.CelluleLumineuse;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author friez
+ */
+public class GrilleDeCellules {
+private CelluleLumineuse[][] matriceCellules;  
+
+public GrilleDeCellules(int lignes, int colonnes) {
+    matriceCellules = new CelluleLumineuse[lignes][colonnes];
+    for (int i = 0; i < lignes; i++) {
+        for (int j = 0; j < colonnes; j++) {
+            matriceCellules[i][j] = new CelluleLumineuse();
+        }
+    }
+}
+
+ public void activerLigneDeCellules(int idLigne) { // Méthode pour activer une ligne de cellules
+        for (int j = 0; j < matriceCellules[idLigne].length; j++) {
+            matriceCellules[idLigne][j].allumer();
+        }
+    }
+ 
+  public void activerColonneDeCellules(int idColonne) { // Méthode pour activer une colonne de cellules
+        for (int i = 0; i < matriceCellules.length; i++) {
+            matriceCellules[i][idColonne].allumer();
+        }
+    }
+  
+    public void activerDiagonaleDescendante() { // Méthode pour activer la diagonale descendante
+        for (int i = 0; i < matriceCellules.length; i++) {
+            matriceCellules[i][i].allumer();
+        }
+    }
+    
+    public void activerDiagonaleMontante() {  // Méthode pour activer la diagonale montante
+        int n = matriceCellules.length;
+        for (int i = 0; i < n; i++) {
+            matriceCellules[i][n - i - 1].allumer();
+        }
+    }
+    
+    public void activerLigneColonneOuDiagonaleAleatoire() { // Méthode pour activer une ligne, colonne ou diagonale aléatoire
+        Random random = new Random();
+        int choix = random.nextInt(4);
+        
+        switch (choix) {
+            case 0:
+                int ligne = random.nextInt(matriceCellules.length);
+                activerLigneDeCellules(ligne);
+                break;
+            case 1:
+                int colonne = random.nextInt(matriceCellules[0].length);
+                activerColonneDeCellules(colonne);
+                break;
+            case 2:
+                activerDiagonaleDescendante();
+                break;
+            case 3:
+                activerDiagonaleMontante();
+                break;
+        }
+    }
+    
+    
+    public void melangerMatriceAleatoirement(int nbTours) { // Méthode pour mélanger la grille
+        Random random = new Random();
+        int lignes = matriceCellules.length;
+        int colonnes = matriceCellules[0].length;
+
+        for (int tour = 0; tour < nbTours; tour++) {
+            int ligneOuColonne = random.nextInt(2);
+            if (ligneOuColonne == 0) {
+                int ligne = random.nextInt(lignes);
+                activerLigneDeCellules(ligne);
+            } else {
+                int colonne = random.nextInt(colonnes);
+                activerColonneDeCellules(colonne);
+            }
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+public String toString() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < matriceCellules.length; i++) {
+        for (int j = 0; j < matriceCellules[i].length; j++) {
+            sb.append(matriceCellules[i][j].estAllumee() ? "X" : "O").append(" ");
+        }
+        sb.append("\n");
+    }
+    return sb.toString();
+}
+}
